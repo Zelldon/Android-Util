c7bb09160401ebc66656ee327432f829359481e5
zell.christopher <zelldon91@gmail.com>
2015-01-30 15:10:45 +0100

    added method which restores the entity list from the given bundle also added new argument key entities to forward the entities to the entity list fragment

--- Zell-Android-Util/src/de/zell/android/util/fragments/EntityListFragment.java
+++ Zell-Android-Util/src/de/zell/android/util/fragments/EntityListFragment.java
@@ -28,55 +28,53 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Represents an abstract EntityListFragment which provides
- * some list fragment functionality and also defines some methods
- * to create a good looking sectional list fragment.
- * 
+ * Represents an abstract EntityListFragment which provides some list fragment
+ * functionality and also defines some methods to create a good looking
+ * sectional list fragment.
+ *
  * @author Christopher Zell <zelldon91@googlemail.com>
  */
 public abstract class EntityListFragment extends ListFragment {
-  
+
   /**
    * The argument key for the entity url.
    */
   public static final String ARG_ENTITIES_URL = "entities.url";
-  
+  /**
+   * The argument key for the entities.
+   */
+  public static final String ARG_ENTITIES = "entities";
   /**
    * The tag key for the entities content.
    */
   private static final String TAG_ENTITIES_CONTENT = "entities.content";
-  
   /**
    * The index of the current showed entity.
    */
   private int index = -1;
-  
   /**
    * The top of the list view.
    */
   private int top = 0;
-  
   /**
-   * The url which identifies the content of the entities. 
+   * The url which identifies the content of the entities.
    */
   private String url;
-  
   /**
    * The list of entities which are showed in the list fragment.
    */
   protected List<Entity> entities = null;
-  
+
   /**
-   * Returns the url which identifies the content of the entities.
-   * The url is used to download the content.
-   * 
+   * Returns the url which identifies the content of the entities. The url is
+   * used to download the content.
+   *
    * @return the url
    */
   public String getURL() {
     return url;
   }
-  
-  
+
   @Override
   public void onResume() {
     super.onResume();
@@ -92,88 +90,93 @@ public abstract class EntityListFragment extends ListFragment {
     View v = this.getListView().getChildAt(0);
     top = (v == null) ? 0 : v.getTop();
   }
-  
-  
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    if (null == savedInstanceState) {
+    if (savedInstanceState == null) {
       savedInstanceState = getArguments();
     }
 
-    if (null != savedInstanceState) {
-      Entity[] e = (Entity[]) savedInstanceState.getSerializable(TAG_ENTITIES_CONTENT);
-      if (e != null)
-        entities = Arrays.asList(e);
-    }
-
-    EntityListAdapter adapter = getEntityListAdapter(getActivity());
-    
-    if (entities == null) {
-      url = (String) savedInstanceState.getSerializable(ARG_ENTITIES_URL);
-      loadEntities();
-    } else
+    if (savedInstanceState != null) {
+      restoreEntities(savedInstanceState, TAG_ENTITIES_CONTENT);
+      EntityListAdapter adapter = getEntityListAdapter(getActivity());
+      if (entities == null) {
+        restoreEntities(savedInstanceState, ARG_ENTITIES);
+        if (entities == null) {
+          url = (String) savedInstanceState.getSerializable(ARG_ENTITIES_URL);
+          loadEntities();
+        } 
+      }
       adapter.setEntities(entities);
-    
-    
-    setListAdapter(adapter);
+      setListAdapter(adapter);
+    }
   }
 
-  
+  /**
+   * Restores the entity list from the given bundle with the given key.
+   * 
+   * @param savedInstanceState the bundle which should contain the entity array
+   * @param key the key which identifies the entity array in the bundle
+   */
+  private void restoreEntities(Bundle savedInstanceState, String key) {
+    Entity[] e = (Entity[]) savedInstanceState.getSerializable(key);
+    if (e != null) {
+      entities = Arrays.asList(e);
+    }
+  }
+
   @Override
   public void onListItemClick(ListView l, View v, int position, long id) {
     super.onListItemClick(l, v, position, id);
     Object item = getListAdapter().getItem(position);
-    
-    if (item instanceof Entity) 
-      onEntityClick(((Entity)item));
-    else
+
+    if (item instanceof Entity) {
+      onEntityClick(((Entity) item));
+    } else {
       onSectionClick(item);
+    }
   }
-  
-  
+
   @Override
   public void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
     outState.putSerializable(TAG_ENTITIES_CONTENT,
-                              entities.toArray(new Entity[entities.size()]));
+            entities.toArray(new Entity[entities.size()]));
   }
-  
+
   /**
-   * The method is called if the element, which was clicked in the list fragment,
-   * was an entity.
-   * 
+   * The method is called if the element, which was clicked in the list
+   * fragment, was an entity.
+   *
    * @param e the entity which was clicked
    */
   protected abstract void onEntityClick(Entity e);
-  
+
   /**
-   * The method is called if the element, which was clicked in the list fragment,
-   * was a section.
-   * 
+   * The method is called if the element, which was clicked in the list
+   * fragment, was a section.
+   *
    * @param o the section which was clicked
    */
   protected abstract void onSectionClick(Object o);
-  
+
   /**
-   * Contains the functionality to load the entities into
-   * the current list adapter.
-   * 
-   * Could contain an async task to load the content from a web resource
-   * or load content from a database.
+   * Contains the functionality to load the entities into the current list
+   * adapter.
+   *
+   * Could contain an async task to load the content from a web resource or load
+   * content from a database.
    */
   protected abstract void loadEntities();
-  
+
   /**
-   * Returns the EntityListAdapter implementation which should be used for 
-   * the list fragment.
-   * 
+   * Returns the EntityListAdapter implementation which should be used for the
+   * list fragment.
+   *
    * @param ctx the application context
    * @return the list adapter which should be used
    */
   protected abstract EntityListAdapter getEntityListAdapter(Context ctx);
-  
-  
-  
 }
