698adeecd9bbb34fa9bab16c586560ab3b790b59
zell.christopher <zelldon91@gmail.com>
2015-04-20 15:52:07 +0200

    Refactor unmarshaller
     * renamed Object instance
     * moved field value cast and setting in own method
     * added docu

--- Zell-Android-Util/src/de/zell/android/util/json/JSONUnmarshaller.java
+++ Zell-Android-Util/src/de/zell/android/util/json/JSONUnmarshaller.java
@@ -29,21 +29,22 @@ import org.json.JSONObject;
 /**
  * The JSONUnmarshaller unmarshalls the given JSONObject object to an object.
  * Which object corresponds to the JSONObject is identified via a class object.
- * The given class should contain JSONElement annotations.
- * If the class contains such an annotation the corresponding field
- * will be filled with the corresponding JSON value.
- * 
+ * The given class should contain JSONElement annotations. If the class contains
+ * such an annotation the corresponding field will be filled with the
+ * corresponding JSON value.
+ *
  * @see JSONElement
  * @see JSONMarshaller
  * @author Christopher Zell <zelldon91@googlemail.com>
  */
 public class JSONUnmarshaller {
+
   private static final String EXCEPTION_CAST_MESSAGE = "Value for field '%1$s' can not be cast from %2$s to %3$s";
 
   /**
-   * Unmarshalls the given JSON object and creates with the given values and class
-   * the corresponding object instance which contains the JSON values. The class
-   * fields must be annotated with the JSONElement annotation to get the
+   * Unmarshalls the given JSON object and creates with the given values and
+   * class the corresponding object instance which contains the JSON values. The
+   * class fields must be annotated with the JSONElement annotation to get the
    * corresponding JSON object values.
    *
    * @see JSONElement
@@ -53,51 +54,70 @@ public class JSONUnmarshaller {
    * @return the instance with the JSON values from type O
    */
   public static <O> O unmarshall(JSONObject json, Class<O> c) {
-    O o = null;
+    O instance = null;
     try {
-      o = c.newInstance();
+      instance = c.newInstance();
       Field fields[] = c.getDeclaredFields();
-      for (Field f : fields) {
-        f.setAccessible(true);
-        JSONElement eleAnno = f.getAnnotation(JSONElement.class);
+      for (Field field : fields) {
+        field.setAccessible(true);
+        JSONElement eleAnno = field.getAnnotation(JSONElement.class);
         if (eleAnno != null) {
           Object value = json.opt(eleAnno.name());
           if (value != null) {
-            if (value instanceof JSONArray)
-              value = unmarshallJSONArray((JSONArray) value, f, o);
-            else if (value instanceof  JSONObject)
+            if (value instanceof JSONArray) {
+              value = unmarshallJSONArray((JSONArray) value, field, instance);
+            } else if (value instanceof JSONObject) {
               value = unmarshall((JSONObject) value, c);
-            try {
-              f.set(o, f.getType().cast(value));
-            } catch (ClassCastException cce) {
-              if (f == null)
-                throw cce;
-              else 
-                throw new ClassCastException(String.format(EXCEPTION_CAST_MESSAGE,
-                                            f.getName(),
-                                            value.getClass().getName(),
-                                            f.getType()));
             }
+            setValueToField(field, value, instance);
           }
         }
-        f.setAccessible(false);
+        field.setAccessible(false);
       }
     } catch (InstantiationException ex) {
       Logger.getLogger(JSONUnmarshaller.class.getName()).log(Level.SEVERE, null, ex);
     } catch (IllegalAccessException ex) {
       Logger.getLogger(JSONUnmarshaller.class.getName()).log(Level.SEVERE, null, ex);
     }
-    return o;
+    return instance;
   }
 
   /**
-   * Unmarshalls a JSON array and returns a corresponding collection object.
-   * The JSON objects in the array are parsed recursively by the public parseJSON method.
+   * Sets the given value to the instance field, the value
+   * will be cast to the field type if possible. If a class cast exception
+   * appears a detailed message will be added.
    * 
+   * @param f the field which gets the new value
+   * @param value the value of the field
+   * @param instance the instance of the class on which the field belongs
+   * @throws IllegalArgumentException 
+   * @throws IllegalAccessException 
+   */
+  private static void setValueToField(Field f, Object value, Object instance)
+          throws IllegalArgumentException, IllegalAccessException {
+    try {
+      f.set(instance, f.getType().cast(value));
+    } catch (ClassCastException cce) {
+      if (f == null) {
+        throw cce;
+      } else {
+        throw new ClassCastException(String.format(EXCEPTION_CAST_MESSAGE,
+                f.getName(),
+                value.getClass().getName(),
+                f.getType()));
+      }
+    }
+  }
+
+  /**
+   * Unmarshalls a JSON array and returns a corresponding collection object. The
+   * JSON objects in the array are parsed recursively by the public parseJSON
+   * method.
+   *
    * @param array the array which contains the values
    * @param f the field which should is the corresponding collection
-   * @param instance the instance of the class which will be filled with the 
-   *                 values of the JSON objects
+   * @param instance the instance of the class which will be filled with the
+   * values of the JSON objects
    * @return the created collection which contains the JSON array values
    */
   private static Object unmarshallJSONArray(JSONArray array, Field f, Object instance) {
@@ -122,7 +142,7 @@ public class JSONUnmarshaller {
       } catch (IllegalArgumentException ex) {
         Logger.getLogger(JSONUnmarshaller.class.getName()).log(Level.SEVERE, null, ex);
       }
-    } 
+    }
     return c;
   }
 }
